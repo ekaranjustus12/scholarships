2f5db3459d1bac7133700d6c8339b44398acebec
Ekaran justus <127421737+ekaranjustus12@users.noreply.github.com>
2026-05-04 15:11:20 -0700

    Update scraper.py

--- scholarship/scraper.py
+++ scholarship/scraper.py
@@ -466,5 +466,5 @@ else:
 if not df.empty:
     output_path = "scholarship/scholarship.json"
     with open(output_path, "w") as f:
-    json.dump(data, f, indent=2)
+        json.dump(data, f, indent=2)
    
